a1f9c7c3b6a2243520b95aaf4da07eab3a417770
Tony Ta <tonyta.tt@gmail.com>
2021-03-05 11:04:10 -0800

    Add original cheese pricing implementation

--- lib/cheese_pricer.rb
+++ lib/cheese_pricer.rb
@@ -1,2 +1,24 @@
 class CheesePricer
+  attr_reader :cheese
+
+  def initialize(cheese)
+    @cheese = cheese
+  end
+
+  def price(ounces)
+    subtotal =
+      case cheese
+      when :cheddar
+        ounces * 120
+      when :swiss
+        ounces * 150
+      when :american
+        ounces * 80
+      else
+        ounces * 100
+      end
+
+    tax = Integer(subtotal * 0.1)
+    subtotal + tax
+  end
 end
--- spec/cheese_pricer_spec.rb
+++ spec/cheese_pricer_spec.rb
@@ -1,3 +1,21 @@
-RSpec.describe CheesePricer do
-  it { should be_a described_class }
+RSpec.describe CheesePricer, "#pricer" do
+  specify "10 oz of cheddar should cost $13.20" do
+    cheese = described_class.new(:cheddar)
+    expect(cheese.price(10)).to eq 1320
+  end
+
+  specify "10 oz of swiss should cost $16.50" do
+    cheese = described_class.new(:swiss)
+    expect(cheese.price(10)).to eq 1650
+  end
+
+  specify "10 oz of american should cost $8.80" do
+    cheese = described_class.new(:american)
+    expect(cheese.price(10)).to eq 880
+  end
+
+  specify "10 oz of other cheeses should cost $11.00" do
+    cheese = described_class.new(:gouda)
+    expect(cheese.price(10)).to eq 1100
+  end
 end
